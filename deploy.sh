0fc02f9ad15b7ca4584358f7a504050aae500a98
ranya-m <ranya.majdi@gmail.com>
2023-01-26 12:52:09 +0100

    3

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:ranya-m/portfolio.git gh-pages
+git push -f git@github.com:ranya-m/portfolio-vite.git gh-pages
 
 cd -
